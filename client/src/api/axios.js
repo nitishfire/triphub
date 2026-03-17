b892fb142a44072946106e276e2d05ece177fba0
nitishfire <nitish.mudaliar125@gmail.com>
2026-03-17 12:42:38 +0000

    Update API base URL for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } });
+//const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } });
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+});
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('triphub_token');
